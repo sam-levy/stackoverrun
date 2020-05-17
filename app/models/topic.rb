431cee05e51affc86e8943cb8c8ea02267080492
Samuel Levy <samulevy@gmail.com>
2020-05-17 17:25:04 -0300

    Add question model and table

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,5 @@
 class Topic < ApplicationRecord
   validates :title, presence: true, uniqueness: true, length: {minimum: 2}
+
+  has_many :questions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_195300) do
+ActiveRecord::Schema.define(version: 2020_05_17_200829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.bigint "topic_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["topic_id"], name: "index_questions_on_topic_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string "title", null: false
@@ -34,4 +45,6 @@ ActiveRecord::Schema.define(version: 2020_05_17_195300) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "topics"
+  add_foreign_key "questions", "users"
 end
